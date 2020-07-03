7a5ee2e4f32ea5994b3b6a340c9758f884dac24f
Stephen Meier <stephen.meier@ibm.com>
2020-07-03 11:58:25 -0400

    Adding an updated basic_model and model training notebook

--- basic_model.py
+++ basic_model.py
@@ -5,42 +5,47 @@ import cv2 as cv
 import requests
 from fastai.vision import *
 from fastai.metrics import error_rate
-import torch
 from fastai.vision import Image
-import matplotlib.pyplot as plt
+from time import sleep
+import numpy as np
+
+
 
 learn = load_learner('data')
-# faceCascade = cv.CascadeClassifier('haarcascade_frontalcatface.xml')
 cap = cv.VideoCapture('http://raspberrypi.mshome.net:8000/stream.mjpg')
+
+i = 20
+j = 0
+preds_array = [''] * 3
 
 while True:
     ret, frame = cap.read()
-    # gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    # faces = faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-    #img = Image(1 - torch.tensor(np.ascontiguousarray(np.flip(frame, 2)).transpose(2,0,1)))
-    #data = ImageList.from_folder(mnist).split_by_folder().label_from_folder().add_test_folder('test').transform(tfms, size=32).databunch().normalize(imagenet_stats)
-    #img.apply_tfms()
-    img = Image(pil2tensor(frame, np.float32).div_(225))
 
-    #print(img)
-    pred = learn.predict(img, thresh=0.6)
-    print(pred[0], pred[2][pred[1].item()])
-    #print(learn.data.classes[pred[0][0]])
+    if i > 0:
+        i -= 1
+        continue
+    i = 20
+
+    pred = learn.predict(Image(pil2tensor(np.flip(frame, 2), np.float32).div_(225)))
+    pred_cat = str(pred[0])
+    pred_prob = float(pred[2][pred[1].item()])
+
+    if (pred_prob > .8) and ((pred_cat == 'java') or (pred_cat == 'python')):
+        pred = pred_cat
+    else:
+        pred = ''
+    preds_array[j % 3] = pred
+    j += 1
     
     cat_found = False
-    # for (x, y, w, h) in faces:
-    #     cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    #     cat_found = True
-    #cv.imshow('frame', frame)
-    ax = plt.subplot()
-    img.show(ax=ax)
-    plt.show()
-    #plt.imshow(img)
-    break
+    print(np.array(preds_array), pred_prob, pred_cat)
+    if np.all(np.array([cat == pred_cat for cat in preds_array])):
+        cat_found = True
     
     if cat_found:
         requests.get('http://raspberrypi.mshome.net:8000/see_cat')
 
+    cv.imshow('frame', frame)
     if (cv.waitKey(1) & 0xFF) == ord('q'):
         cap.release()
         cv.destroyAllWindows()
